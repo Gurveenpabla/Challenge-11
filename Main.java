533aff5e57fa50d7c4f51237b9e58f5252cb85ef
vmsoGroup3 <soteched2017+group3@gmail.com>
2020-09-29 17:52:10 +0000

    test

--- Main.java
+++ Main.java
@@ -6,6 +6,19 @@ class Main {
   }
 }
 
-if you click on version control, there's an option to create a git rep
-oh ok 
-did someone create it? 
+/*
+# example 1 - create a git repo from an existing directory
+git init # turn current directory into a git repo
+
+# example 2 - clone a remote repository 
+git clone [url] 
+did someone connect their github..? i dont know but how do we connect it ohh isee
+okay xd
+
+
+/*
+# configure git user information 
+git config --global user.name "vmso" # virtual mr. so
+git config --global user.email "soteched2017@gmail.com" # disposable email address
+git config --global color.ui auto
+git config -l # lists all configured
