0386ab4a14c066278383996c50139742101e8124
vmsoGroup3 <soteched2017+group3@gmail.com>
2020-09-29 17:32:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+
+if you click on version control, there's an option to create a git rep
+oh ok 
